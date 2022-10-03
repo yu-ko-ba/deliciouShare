621a5e006acbf7c9205f251381e5f1eaf0a056ce
tacg0909 <97862228+tacg0909@users.noreply.github.com>
2022-10-03 15:56:27 +0900

    Update apiUrls.ts

--- src/utils/apiUrls.ts
+++ src/utils/apiUrls.ts
@@ -1,5 +1,5 @@
 const apiUrls = {
-  getUserPostsUrl: process.env.NEXT_PUBLIC_GET_USER_POSTS_URL || Cypress.env("get_user_posts_url")
+  getUserPostsUrl: process.env.NEXT_PUBLIC_GET_USER_POSTS_URL || Cypress.env("get_user_posts_url") || ""
 }
 
 export default apiUrls
