bbf143a0f965268c560ca7f1244d9420de453005
yu-ko-ba <yu-ko-ba@outlook.com>
2022-10-18 15:36:11 +0900

    投稿の概要を取得する関数を作った

---
+++ src/utils/fetchUserPostOutlinesData.ts
@@ -0,0 +1,14 @@
+import fetchUserPostOutlines from "./fetchUserPostOutlines"
+
+type UserPostOutline = {
+  postedTime: string,
+  smallImageUrl: string,
+  postId: string,
+}
+
+const fetchUserPostOutlinesData = async (userId: string) => {
+  return await fetchUserPostOutlines(userId)
+    .then((response) => response.data as UserPostOutline[])
+}
+
+export default fetchUserPostOutlinesData
