9f8f5781f71b3d014a1c5a4519a1f85d6d9e5d57
yu-ko-ba <yu-ko-ba@outlook.com>
2022-10-24 14:58:51 +0900

    ログインできるようにした

---
+++ src/pages/settings.tsx
@@ -0,0 +1,49 @@
+import { LoadingButton } from "@mui/lab"
+import { Container, ThemeProvider } from "@mui/material"
+import { Auth } from "aws-amplify"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import MeshiteroAppBar from "../components/MeshiteroAppBar"
+import theme from "../theme"
+
+const Settings = () => {
+  const router = useRouter()
+
+  const [signOutButtonIsLoadingNow, setSignOutButtonIsLoadingNow] = useState(false)
+  return (
+    <ThemeProvider theme={theme}>
+      <MeshiteroAppBar />
+      <Container maxWidth="xs">
+        <LoadingButton
+          variant="outlined"
+          loading={signOutButtonIsLoadingNow}
+          onClick={() => {
+            setSignOutButtonIsLoadingNow(true)
+            Auth.signOut()
+              .then(() => {
+                router.push("sign-in")
+              })
+              .catch((err) => {
+                console.log(err)
+                setSignOutButtonIsLoadingNow(false)
+                  })
+          }}
+          fullWidth
+        >
+          ログアウト
+        </LoadingButton>
+        <LoadingButton
+          variant="outlined"
+          onClick={() => {
+            Auth.signOut({ global: true })
+          }}
+          fullWidth
+        >
+          全ての端末でログアウト
+        </LoadingButton>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default Settings
