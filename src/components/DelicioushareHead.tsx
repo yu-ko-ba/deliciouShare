4165baf0848f98b01c1240a4d8309e3e7da876aa
yu-ko-ba <yu-ko-ba@outlook.com>
2022-11-04 09:37:51 +0900

    Headコンポーネントを作った #56

---
+++ src/components/DelicioushareHead.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head"
+import { useRouter } from "next/router"
+import React from "react"
+
+type Props = {
+  title?: string
+  description?: string
+  imageUrl?: string
+  ogType?: "website" | "article"
+  twitterCardType?: "app" | "summary_large_image" | "summary" | "player"
+}
+
+const DelicioushareHead = ({
+  title = "deliciouShare.app",
+  description = "おいしい！をシェアしよう",
+  imageUrl = "",
+  ogType = "website",
+  twitterCardType,
+}: Props) => {
+  const router = useRouter()
+
+  return (
+    <Head>
+      <meta name="theme-color" content="#bbdefb" />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:url" content={router.pathname} />
+      {twitterCardType && (
+        <>
+          <meta name="twitter:card" content={twitterCardType} />
+          <meta name="twitter:creator" content="@deliciouShare" />
+        </>
+      )}
+    </Head>
+  )
+}
+
+export default DelicioushareHead
